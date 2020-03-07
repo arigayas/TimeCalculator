82609ad7f7bd8e939dfee6c77750636fe3a61e37
arigayas+git@gmail.com <arigayas>
2020-03-08 00:40:05 +0900

    ファイルの修整、改行コードの変更

--- TimeCalculator.dpr
+++ TimeCalculator.dpr
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 program TimeCalculator;
 
 uses
@@ -13,19 +12,3 @@ begin
   Application.CreateForm(TForm1, Form1);
   Application.Run;
 end.
-=======
-program TimeCalculator;
-
-uses
-  Vcl.Forms,
-  Unit1 in 'Unit1.pas' {Form1};
-
-{$R *.res}
-
-begin
-  Application.Initialize;
-  Application.MainFormOnTaskbar := True;
-  Application.CreateForm(TForm1, Form1);
-  Application.Run;
-end.
->>>>>>> dda96ccedea90781c4c33c18dd91c2950be9600a
